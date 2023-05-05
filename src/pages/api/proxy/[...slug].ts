3c04ac9b8d7b93708bb103cd23529f70f2b7f429
gslnzfq <gslnzfq@live.cn>
2023-05-05 15:41:02 +0800

    fix: 修复readme中图片无法展示的问题

--- src/pages/api/proxy/[...slug].ts
+++ src/pages/api/proxy/[...slug].ts
@@ -14,7 +14,6 @@ export default function handler(
 ) {
   const slug = req.query.slug as string[];
   const [target, ...url] = slug as [string, string];
-
   if (!url.length) {
     res.status(400).json({msg: "参数不能为空"});
     return;
@@ -28,9 +27,16 @@ export default function handler(
   console.log("requestUrl", requestUrl);
 
   const isJSON = requestUrl.endsWith(".json");
+  const isMD = requestUrl.endsWith(".md");
   axios.get(requestUrl).then(response => {
     if (isJSON) {
       return res.status(response.status).json(response.data);
+    }
+    if (isMD) {
+      // 替换https://github.com开头的图片资源为本站代理的的图片
+      response.data = response.data.replace(/https:\/\/github.com\/(\S*?)\/(\S*?)\/blob\/(.*)\)/g, function (match: string, user: string, repos: string, path: string) {
+        return `${req.headers["x-forwarded-proto"]}://${req.headers["host"]}/api/proxy/raw/${user}/${repos}/${path})`;
+      });
     }
     res.status(response.status).end(response.data);
   });
