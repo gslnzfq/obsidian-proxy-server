fb952505cbb0b59b8cea418356c3bc75a54b1448
gslnzfq <gslnzfq@live.cn>
2023-08-11 09:27:35 +0800

    chore: 图片放到本地，更新网页

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,12 @@
-import { Html, Head, Main, NextScript } from "next/document";
-import { Meta } from "next/dist/lib/metadata/generate/meta";
+import {Html, Head, Main, NextScript} from "next/document";
+import {Meta} from "next/dist/lib/metadata/generate/meta";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
         <Meta content="width=device-width, initial-scale=1" name="viewport"/>
+        <title>Obsidian代理服务</title>
       </Head>
       <body>
       <Main/>
